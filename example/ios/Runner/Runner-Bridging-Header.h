3d249788f5bf9aa91bcd0282ac112c69cb8fa17b
“Betcha” <betcha.global@gmail.com>
2024-11-12 07:51:44 -0500

    swift header

--- example/ios/Runner/Runner-Bridging-Header.h
+++ example/ios/Runner/Runner-Bridging-Header.h
@@ -1,3 +1,3 @@
 #import "GeneratedPluginRegistrant.h"
 // Runner-Bridging-Header.h
-#import "flutter_braintree/flutter_braintree-Swift.h"
+#import "flutter_braintree_betc/flutter_braintree_betc-Swift.h"
